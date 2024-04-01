f3c2a18d1fba0020153a3da4b0005da6a5dd4a97
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-04-01 11:55:36 -0300

    :rocket: feat: altera baseURL api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://api-cadastro-cliente.vercel.app/'
+    baseURL: 'https://api-cadastro-cliente.vercel.app/api/',
 })
