a24f19bb4eb46d00043eef779929bd115214c33c
WANDERLEY CABRAL <39313251+cabraldasilvac@users.noreply.github.com>
2024-04-11 09:26:04 -0300

    Update api.ts - Altera URL api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://api-cadastro-cliente.vercel.app/api/',
+    'https://api-cadastro-cliente-roan.vercel.app/api/',
+    // baseURL: 'https://api-cadastro-cliente.vercel.app/api/',
 })
