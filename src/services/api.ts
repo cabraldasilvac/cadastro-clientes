1e62efa9a6a4d1c3a3b5341305fe42e1e9eec4c6
WANDERLEY CABRAL <39313251+cabraldasilvac@users.noreply.github.com>
2024-04-11 09:27:54 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    'https://api-cadastro-cliente-roan.vercel.app/api/',
+    baseURL: 'https://api-cadastro-cliente-roan.vercel.app/api/',
     // baseURL: 'https://api-cadastro-cliente.vercel.app/api/',
 })
